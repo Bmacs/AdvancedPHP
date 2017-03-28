908f172b3d8b0cb41eae13f08f1618961872f405
Brendan McNamara <Brendancmcnamara@gmail.com>
2017-03-27 22:19:07 -0400

    Add files via upload

---
+++ week1/lab/models/validation.php
@@ -0,0 +1,23 @@
+<?php 
+
+function isValidEmail($email) {
+	if (filter_var($email, FILTER_VALIDATE_EMAIL) !== false ) {
+		return true;
+	}
+
+	return false;
+}
+
+function isValidZIP($zip) {
+	$zipREGEX = '/^[0-9]{5}$/';
+
+	if ( !preg_match($zipREGEX, $zip) ) {
+		return false;
+	}
+
+	return true;
+}
+
+
+
+?>
