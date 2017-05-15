38cee70ea452759721389b563317d27dc85ffc2d
Brendan McNamara <Brendancmcnamara@gmail.com>
2017-05-15 16:09:46 -0400

    Add files via upload

--- week4/lab/index.html.php
+++ week4/lab/index.html.php
@@ -1,37 +1,45 @@
-<table>
-	<tbody>
-		<th>File Number</th>
-		<th>File Uploads</th>
+<div class="container">
+	<table class=".table-condensed">
+		<tbody>
+			<th>File Number</th>
+			<th>File Uploads</th>
 
-		<?php 
-		$fileNum = 0;
+			<?php 
+			$fileNum = 0;
 
-		foreach ($directory as $fileInfo) :
+			foreach ($directory as $fileInfo) :
 
 
 
-			if ($fileInfo->isFile()) :
-			//when something is uploaded increase the number
+				if ($fileInfo->isFile()) :
+				//when something is uploaded increase the number
 
-				$fileNum++;
+					$fileNum++;
 
+
+				?>
+
+						<tr>
+								<td><strong><?php echo $fileNum; ?></strong></td>
+							
+								<td><?php echo $fileInfo->getFileName(); ?>
+								<p><strong>Uploaded on <?php echo date("l F j, Y, g:i a", $fileInfo->getMTime()); ?></strong></p>
+									
+
+								</td>
+						
+								
+								<td><a href='?delete=./uploads/ <?php echo $fileInfo->getFileName(); ?>'>Delete</a></td>
+								<td><a href="<?php echo $fileInfo->getPathname();?>">View</td>
+								<td></td>
+								<td><p>This file is <?php echo $fileInfo->getSize(); ?> byte's</p></td>
+						</tr>
+					
+			<?php 
+			endif;
+			endforeach;
 
 			?>
-
-					<tr>
-							<td><?php echo $fileNum; ?></td>
-						
-							<td><?php echo $fileInfo->getFileName(); ?></td>
-					
-							
-							<td><a href='?delete=./uploads/ <?php echo $fileInfo->getFileName(); ?>'>Delete</a></td>
-							<td><a href="./read.php/">View</td>
-					</tr>
-				
-		<?php 
-		endif;
-		endforeach;
-
-		?>
-	</tbody>
-</table>
+		</tbody>
+	</table>
+</div>	
--- week4/lab/index.php
+++ week4/lab/index.php
@@ -2,7 +2,14 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script></title>
     </head>
     <body>
     <form enctype="multipart/form-data" action="upload.php" method="POST">
@@ -18,7 +25,7 @@
 
     $directory = new DirectoryIterator($folder);
 
-    include './views/index.html.php/';
+    include './index.html.php';
     include 'upload-form.php';
 
          
